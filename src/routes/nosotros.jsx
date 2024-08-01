80facb1628cc20ef98e27cf32670ce8677e9a45b
Giuli Palavecino <65983075+M4STERPRO@users.noreply.github.com>
2024-08-01 14:41:44 -0300

    Update nosotros.jsx

--- src/routes/nosotros.jsx
+++ src/routes/nosotros.jsx
@@ -4,7 +4,6 @@ function Nosotros () {
        <p className='text-2xl max-w-[600px] mb-20'>
           Checkpoint es una página que brinda su servicio a instituciones para facilitar el registro de asistencias dentro de las mismas. Se utiliza una base de datos para cargar de forma sencilla la información necesaria de cada identidad.
           ¿Qué esperas? ¡Aprovecha al máximo las grandes capacidades de Checkpoint!
-          
         </p>
     </main>  
   )
