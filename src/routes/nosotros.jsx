f20b96aa219f1152c95d2459a804d40a975fa960
Giuli Palavecino <65983075+M4STERPRO@users.noreply.github.com>
2024-08-01 16:04:35 -0300

    Update nosotros.jsx

--- src/routes/nosotros.jsx
+++ src/routes/nosotros.jsx
@@ -9,9 +9,6 @@ export default function ContactPage () {
           <p className='text-2xl max-w-[600px] mb-4'>
             Si tienes dudas o necesitas ayuda, no dudes en contactarnos.
           </p>
-          <p className='text-slate-500 text-2xl'>mberardi@etrr.edu.ar</p>
-          <p className='text-slate-500 text-2xl'>ifreitag@etrr.edu.ar</p>
-          <p className='text-slate-500 text-2xl'>gpalavecino@etrr.edu.ar</p>
 
           <div className='flex gap-4 mt-20'>
             <Link to='/nosotros' className='font-bold bg-slate-300 hover:bg-slate-400 px-4 py-2 rounded-md'>
