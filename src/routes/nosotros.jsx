f13f4e6ff2f43202cbf328d45595bbf2e7eedae8
Giuli Palavecino <65983075+M4STERPRO@users.noreply.github.com>
2024-08-01 16:31:18 -0300

    Update nosotros.jsx

--- src/routes/nosotros.jsx
+++ src/routes/nosotros.jsx
@@ -7,8 +7,9 @@ export default function ContactPage () {
       <div className='flex'>
         <section className=''>
           <p className='text-2xl max-w-[600px] mb-4'>
-            Checkpoint es una página que brinda su servicio a instituciones para facilitar el registro de asistencias dentro de las mismas.
-              Se utiliza una base de datos para cargar de forma sencilla la información necesaria de cada identidad.
+            Checkpoint es una página que brinda su servicio a instituciones para facilitar el registro de asistencias dentro de las mismas.<br>
+            Se utiliza una base de datos para cargar de forma sencilla la información necesaria de cada identidad.<br>
+            ¿Qué esperas? ¡Aprovecha al máximo las grandes capacidades de Checkpoint!
           </p>
 
           <div className='flex gap-4 mt-20'>
