4421447ace3a6a01df9f75f2bec8f547a7d40e97
Giuli Palavecino <65983075+M4STERPRO@users.noreply.github.com>
2024-08-01 15:07:00 -0300

    Update nosotros.jsx

--- src/routes/nosotros.jsx
+++ src/routes/nosotros.jsx
@@ -1,11 +1,12 @@
 function Nosotros () {
   return (
     <main>
-       <p className='text-2xl max-w-[600px] mb-20'>
-          Checkpoint es una página que brinda su servicio a instituciones para facilitar el registro de asistencias dentro de las mismas.
-         Se utiliza una base de datos para cargar de forma sencilla la información necesaria de cada identidad.
+      <p className='text-2xl max-w-[600px] mb-20'>
+        Checkpoint es una página que brinda su servicio a instituciones para facilitar el registro de asistencias dentro de las mismas.
          ¿Qué esperas? ¡Aprovecha al máximo las grandes capacidades de Checkpoint!
         </p>
+      <p className='text-2xl max-w-[600px] mb-20'>
+        Se utiliza una base de datos para cargar de forma sencilla la información necesaria de cada identidad.</p>
     </main>  
   )
 }
