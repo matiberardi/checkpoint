0802a264781f39c853268d6279684e61f611fcd8
giuli <gpalavecino@etrr.edu.ar>
2024-08-02 15:39:40 -0300

    as

--- src/components/preceptorComponents/cursos.jsx
+++ src/components/preceptorComponents/cursos.jsx
@@ -20,7 +20,6 @@ function Cursos ({ setPage, setCurso }) {
   return (
     <div>
       <h1 className='text-3xl mb-4'>Cursos</h1>
-      {/*<ul className='grid grid-cols-2 gap-2 w-fit'>*/}
       <ul className='grid grid-cols-4 gap-4 w-fit'>
         {cursos.map((curso) => (
           <li
