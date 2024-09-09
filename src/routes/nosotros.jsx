67fff1c755c5713d2f0a49ac497cc4cadbfe9c6a
Giuli Palavecino <65983075+M4STERPRO@users.noreply.github.com>
2024-09-09 16:27:36 -0300

    Update nosotros.jsx

--- src/routes/nosotros.jsx
+++ src/routes/nosotros.jsx
@@ -27,7 +27,7 @@ export default function ContactPage () {
         </section>
         <img
           className='w-full max-w-[600px]'
-          src='https://i.imghippo.com/files/84Jh51722602676.png'
+          src='https://i.imghippo.com/files/gInwf1725909470.png'
           alt='Logo'
         />
       </div>
